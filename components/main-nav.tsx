f288fbea9745945285465be53f0b47374c5173ec
Aestheticsuraj234 <jhapriya458@gmail.com>
2023-09-18 14:31:30 +0530

    little fix

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -46,17 +46,17 @@ export function MainNav({
         )}
       >
         <DropdownMenu>
-          <DropdownMenuTrigger className={cn( 
-            
+          <DropdownMenuTrigger className={cn(
+
             'text-sm font-medium flex-row flex items-center justify-center gap-1 transition-colors outline-none hover:text-primary',
             pathname === "#" ? 'text-black dark:text-white' : 'text-muted-foreground'
 
           )}>About <ChevronDown size={18} /></DropdownMenuTrigger>
           <DropdownMenuContent className="flex flex-1 justify-center items-start  flex-col">
-           
-           <Link href={"/About/Company-Profile"}><DropdownMenuItem className="flex  w-full justify-center items-start gap-3"> <Building2 size={18} className="text-zinc-600" /> Company Profile</DropdownMenuItem></Link> 
-           <Link href={"/About/Our-Team"}><DropdownMenuItem className="flex w-full justify-center items-start gap-3"><Rocket size={18} className="text-zinc-600"/> Our Team</DropdownMenuItem></Link>
-           <Link href={"/About/Our-Mission-Vision"}> <DropdownMenuItem className="flex  w-full justify-center items-start gap-3"><Users size={18} className="text-zinc-600" />  Our Mission & Vision</DropdownMenuItem></Link>
+
+            <Link href={"/About/Company-Profile"}><DropdownMenuItem className="flex  w-full justify-center items-start gap-3"> <Building2 size={18} className="text-zinc-600" /> Company Profile</DropdownMenuItem></Link>
+            <Link href={"/About/Our-Team"}><DropdownMenuItem className="flex w-full justify-center items-start gap-3"><Rocket size={18} className="text-zinc-600" /> Our Team</DropdownMenuItem></Link>
+            <Link href={"/About/Our-Mission-Vision"}> <DropdownMenuItem className="flex  w-full justify-center items-start gap-3"><Users size={18} className="text-zinc-600" />  Our Mission & Vision</DropdownMenuItem></Link>
 
           </DropdownMenuContent>
         </DropdownMenu>
